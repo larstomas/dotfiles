3d7ec5467b20e270cdbda2bd5d04aff31069b8c7
Tomas Bäckman <larstomas@gmail.com>
2023-03-08 10:59:15 +0100

    Fix Xcode installation

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 #- Error handling 
 # [set -e, -u, -o, -x pipefail explanation](https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021)
 # -e: exit on error
-set -eufo pipefail
+set -euf -o pipefail
 
 #- Ask for admin password upfront
 sudo -v
@@ -11,29 +11,16 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
-#- Functions
-function command_exists() {
-  # `command -v` is similar to `which`
-  # https://stackoverflow.com/a/677212/1341838
-  command -v $1 >/dev/null 2>&1
-}
-
-
 #- Install Xcode Command Line Tools 
-if !(command_exists xcode-select); then
-  time xcode-select --install
+if [ "$(xcode-select -p)" != "/Library/Developer/CommandLineTools" ]; then
+  xcode-select --install
   echo ">>> Wait for Xcode Command Line Tools to install"
   read -p "Press any key to continue "
 fi
 
 #-- Test xcode installation
-if command_exists xcode-select; then
-  if [ "$(xcode-select -p)" != "/Library/Developer/CommandLineTools" ]; then
-    echo "Installation of Xcode Command Line Tools failed, Xcode path not right"
-    exit 1
-  fi
-else
-  echo "Installation of Xcode Command Line Tools failed, xcode-select command not found"
+if [ "$(xcode-select -p)" != "/Library/Developer/CommandLineTools" ]; then
+  echo "Installation of Xcode Command Line Tools failed, Xcode path not right"
   exit 1
 fi
 echo "Xcode Command Line Tools is installed"
