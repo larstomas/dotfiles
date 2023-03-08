e150ea029c3f92ffd3843cbb25e6af1dc29df0ea
Tomas Bäckman <larstomas@gmail.com>
2023-03-08 09:57:27 +0100

    Remove code-select variable

--- install.sh
+++ install.sh
@@ -1,31 +1,34 @@
 #!/bin/sh
 
+#- Error handling 
 # [set -e, -u, -o, -x pipefail explanation](https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021)
 # -e: exit on error
 set -eufo pipefail
 
-#- ask for admin password upfront
+#- Ask for admin password upfront
 sudo -v
 # Keep-alive: update existing `sudo` time stamp until `.osx` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
+
+#- Functions
 function command_exists() {
   # `command -v` is similar to `which`
   # https://stackoverflow.com/a/677212/1341838
   command -v $1 >/dev/null 2>&1
 }
 
-# Install Xcode Command Line Tools 
+
+#- Install Xcode Command Line Tools 
 if !(command_exists xcode-select); then
   time xcode-select --install
   echo ">>> Wait for Xcode Command Line Tools to install"
   read -p "Press any key to continue "
 fi
 
-# Test xcode installation
+#-- Test xcode installation
 if command_exists xcode-select; then
-  xcode_path=$(xcode-select -p)
-  if [ "$xcode_path" != "/Library/Developer/CommandLineTools" ]; then
+  if [ "$(xcode-select -p)" != "/Library/Developer/CommandLineTools" ]; then
     echo "Installation of Xcode Command Line Tools failed, Xcode path not right"
     exit 1
   fi
@@ -35,14 +38,9 @@ else
 fi
 echo "Xcode Command Line Tools is installed"
 
-#echo "Enter superuser (sudo) password to accept Xcode license"
-#sudo xcodebuild -license accept
-#sudo xcodebuild -runFirstLaunch
 
-
-# install Homebrew with applications
+#- Install Homebrew with applications
 source ./scripts/setup_homebrew_macos.zsh
-
 
 exit 0
 
