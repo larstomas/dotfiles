382aa965e96ec2b9c6aa9fc67eba83eae6d4cf1e
Tomas Bäckman <11527327+larstomas@users.noreply.github.com>
2023-03-09 22:47:32 +0100

    Create mackup-restore-app-settings.zsh

---
+++ scripts/mackup-restore-app-settings.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+#- Error handling 
+# [set -e, -u, -o, -x pipefail explanation](https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021)
+# -e: exit on error
+set -euf -o pipefail
+
+unzip $HOME/Downloads/mackup-backup.zip -d $HOME/.config/
+mackup restore
+mv $HOME/.config/Mackup $HOME/Downloads/
