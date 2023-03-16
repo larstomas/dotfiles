4c8328bcb2d528ad0202f712b07223b2258e8fc4
Tomas Bäckman <larstomas@gmail.com>
2023-03-16 17:17:54 +0100

    before zenseact

--- home/.chezmoiscripts/darwin/run_onchange_before_macos-config.zsh
+++ home/.chezmoiscripts/darwin/run_onchange_before_macos-config.zsh
@@ -26,22 +26,20 @@ echo "# Starting macOS Setup"
 # Close System Preferences
 osascript -e 'tell application "System Preferences" to quit'
 
-#- Set hostnamn
-computername="$(hostname)"
-echo -e "\n  What should your computer be named? (default: $computername)"
-echo -ne "  > \033[34m\a"
-read
-echo -e "\033[0m\033[1A"
-[ -n "$REPLY" ] && computername=$REPLY
+#TODO: make an if statement
+# #- Set hostnamn
+# computername="$(hostname)"
+# echo -e "\n  What should your computer be named? (default: $computername)"
+# echo -ne "  > \033[34m\a"
+# read
+# echo -e "\033[0m\033[1A"
+# [ -n "$REPLY" ] && computername=$REPLY
 
-#-- Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName $computername
-sudo scutil --set HostName $computername
-sudo scutil --set LocalHostName $computername
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $computername
-
-#- Quit System Preferences
-osascript -e 'tell application "System Preferences" to quit'
+# #-- Set computer name (as done via System Preferences → Sharing)
+# sudo scutil --set ComputerName $computername
+# sudo scutil --set HostName $computername
+# sudo scutil --set LocalHostName $computername
+# sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $computername
 
 #- System Preferences (in macOS Ventura 13) 
 #-- Sound
