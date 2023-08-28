419a1009adc87948e0177cdbe6c54350270da50c
Tomas Bäckman <larstomas@gmail.com>
2023-08-28 14:46:55 +0200

    2023-08-28

--- home/.chezmoiscripts/darwin/run_onchange_after_install-packages.zsh
+++ home/.chezmoiscripts/darwin/run_onchange_after_install-packages.zsh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 
-#- Error handling 
+#- Error handling
 # [set -e, -u, -o, -x pipefail explanation](https://gist.github.com/mohanpedala/1e2ff5661761d3abd0385e8223e16425?permalink_comment_id=3945021)
 # -e: exit on error
 set -euf -o pipefail
@@ -15,8 +15,8 @@ sudo -v
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 
-export HOMEBREW_CASK_OPTS="--no-quarantine" 
+export HOMEBREW_CASK_OPTS="--no-quarantine"
 export HOMEBREW_BUNDLE_FILE="$DOTFILES/Brewfile"
 
 # Install apps
-time brew bundle 
+time brew bundle
